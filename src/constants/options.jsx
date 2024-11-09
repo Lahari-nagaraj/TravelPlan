17a94ab819c0c283224374e47da608672c5b17a3
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2024-11-09 18:45:29 +0530

    source file

---
+++ src/constants/options.jsx
@@ -0,0 +1,54 @@
+export const SelectTravelsList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A solo travels in exploration',
+        icon:'✈️',
+        people:'1'
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc:'Two traveles in tandom',
+        icon:'🥂',
+        people:'2 people'  
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving ady',
+        icon:'🏡',
+        people:'3 to 5 people'  
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'A bunch of all thrill-seekers',
+        icon:'⛵',
+        people:'5 to 10 people'    
+    },
+]
+
+export const SelectBudgetOptions=[
+    {
+        id:1,
+        title:'Cheap',
+        desc:'Stay conscious of costs',
+        icon:'💵',
+    }, 
+    {
+        id:2,
+        title:'Moderate',
+        desc:'Keep cost on the average side',
+        icon:'💰',
+           
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:'Dont worry about cost',
+        icon:'💸', 
+    },
+]
+
+export const AI_PROMPT='Genetate Travel plan for Location :{location}'
